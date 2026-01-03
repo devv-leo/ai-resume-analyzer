78e3a26868e75660bf406466c0f724dd427f701e
devv-leo <lekan.code@gmail.com>
2026-01-03 15:31:28 +0100

    authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,9 @@
 import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "../../constants";
+import { useLocation, useNavigate } from 'react-router';
+import { useEffect } from 'react';
+import { usePuterStore } from '~/lib/puter';
 
 export function meta() {
   return [
@@ -8,13 +13,31 @@ export function meta() {
 }
 
 export default function Home() {
+  const { auth } = usePuterStore();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(!auth.isAuthenticated) navigate('/auth?next=/'); // if not logged in (!)
+    }, [auth.isAuthenticated])
+  
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-    <Navbar></Navbar>
+    <Navbar />
     <section className="main-section">
-      <div className="page-heading">
+      <div className="page-heading py-16">
         <h1>Track Your Applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback</h2>
       </div>
+
+      {resumes.length > 0 && (
+      <div className="resumes-section grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
+        {resumes.map((resume) => (
+          <ResumeCard key={resume.id} resume={resume} />
+        ))}
+      </div>
+      
+
+      )}
     </section>
+
   </main>
 }
